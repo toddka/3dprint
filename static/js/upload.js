b494b590938fd5e106032388cf2aff9e2709bcff
toddkashley <toddkashley@gmail.com>
2016-06-28 12:24:49 -0400

    added ajax to handle file uploads. TODO: add bootstrap modal dropdown with success not and then redirect after accept on the modal.

---
+++ static/js/upload.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+	$('form').on('submit', function(event) {
+
+			event.preventDefault();
+
+			var formData = new FormData($('form')[0]);
+
+			$.ajax({
+				xhr : function() {
+					var xhr = new window.XMLHttpRequest();
+
+					xhr.upload.addEventListener('progress', function(e) {
+
+						if (e.lengthComputable) {
+							console.log('Percentage Uploaded: ' + (e.loaded / e.total))
+							var percent = Math.round(e.loaded / e.total * 100)
+
+							$('#progress-bar').css('width', percent + '%');
+							$('button').removeClass("btn btn-primary").addClass("btn btn-primary disabled");
+						}
+
+					});
+
+					return xhr;
+				},
+				type : 'POST',
+				url : '/new',
+				data : formData,
+				processData : false,
+				contentType : false,
+				success : function() {
+					$('#success-note').attr('aria-hidden', false);
+				}
+			});
+	});
+});
