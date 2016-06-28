8c84fe6a9ed69090ff512de47215426263c9eb83
toddkashley <toddkashley@gmail.com>
2016-06-28 10:06:34 -0400

    added file uploading

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 import os
 from flask import Flask, redirect, url_for, request, render_template
 from pymongo import MongoClient
+from werkzeug.utils import secure_filename
+
+
+ALLOWED_EXTENSIONS = set(['.stlt', '.rar', '.mp3'])
 
 app = Flask(__name__)
+app.debug = True
 
-client = MongoClient(
-	os.environ['DB_PORT_27017_TCP_ADDR'], 
-	27017)
+client = MongoClient('mongodb://localhost:27017/')
+# client = MongoClient(
+# 	os.environ['DB_PORT_27017_TCP_ADDR'], 
+# 	27017)
+
 db = client.printdb
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def queue():
@@ -31,18 +41,24 @@ def submit():
 @app.route('/new', methods=['POST'])
 def new():
 
+	f = request.files['file']
+	print f.filename
+	# if f and allowed_file(f.filename):
+	if f:
+		f.save('files/' + request.form['inputCompany'] + "_" + secure_filename(f.filename))
+
 	item = {
 		'name': request.form['inputName'],
 		'company': request.form['inputCompany'],
 		'title': request.form['inputTitle'],
 		'email': request.form['inputEmail'],
 		'printer': request.form['select'],
-		'additional': request.form['inputAdditional']
+		'additional': request.form['inputAdditional'],
 	}
 
 	db.printdb.insert_one(item)
 
-	return redirect(url_for('queue'))
+	return redirect(url_for('view'))
 
 
 if __name__ == "__main__":
